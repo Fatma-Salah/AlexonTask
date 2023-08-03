b9c9accd6ed9a6fa2462a8edf4c5e40b5c4392cb
Fatma-Salah <fatmsalah767@gmail.com>
2023-08-03 17:59:35 +0300

    first commit

--- src/app/components/choosecomplaint/choosecomplaint.component.ts
+++ src/app/components/choosecomplaint/choosecomplaint.component.ts
@@ -4,53 +4,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-choosecomplaint',
   templateUrl: './choosecomplaint.component.html',
-  styleUrls: ['./choosecomplaint.component.css']
+  styleUrls: ['./choosecomplaint.component.css'],
 })
-export class ChoosecomplaintComponent implements OnInit{ 
- 
-  constructor(private router:Router) {}
-  ngOnInit(): void {  
-      if (localStorage.getItem('title') !=' نوع الشكوي ') { 
-        localStorage.setItem('title',' نوع الشكوي '); 
-        location.reload() 
-      } 
-    const complainBoxElements: NodeListOf<HTMLElement> = document.querySelectorAll('.container-body > div');
-complainBoxElements.forEach((element: HTMLElement) => {
-  /// add style to div 
- element.addEventListener('mouseenter',function(){
-    element.classList.add('box-success');
-  }) ;
-  // remove style 
-element.addEventListener('mouseleave',function(){
-  element.classList.remove('box-success');
-}) ;
+export class ChoosecomplaintComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    if (localStorage.getItem('title') != ' نوع الشكوي ') {
+      localStorage.setItem('title', ' نوع الشكوي ');
+      location.reload();
+    }
+    const complainBoxElements: NodeListOf<HTMLElement> =
+      document.querySelectorAll('.container-body > div');
+    complainBoxElements.forEach((element: HTMLElement) => {
 
-// add style to box and button when click
-element.addEventListener('click',function (event) {
-  const clickedBox = event.target;
-  const button =document.querySelector('.container-confirm') as HTMLElement;
-console.log(clickedBox);
-
-  // Add "box-success" class to the clicked box
-  element.classList.add('box-success'); 
-  // Add style to button
-  button.classList.add('able-button');
-
-  // Remove "box-success" class from other boxes 
-    if (element !== clickedBox) {
-      element.classList.remove('box-success');
-    } 
-});
-  
-});
-
-} 
- 
-goToControlPanel(event:any) { 
-  if(event.target.classList.contains('able-button')) { 
-    this.router.navigate(['controlPanel']);
+      // add style to box and button when click
+      element.addEventListener('click', function () {
+        const button = document.querySelector(
+          '.container-confirm'
+        ) as HTMLElement;
+        // Add "box-success" class to the clicked box
+        element.classList.add('box-success');
+        // Add style to button
+        button.classList.add('able-button');
+      });
+    });
   }
-   
-}
- 
+
+  goToControlPanel(event: any) {
+    if (event.target.classList.contains('able-button')) {
+      this.router.navigate(['controlPanel']);
+    }
+  }
 }
--- src/app/components/includes/navbar/navbar.component.ts
+++ src/app/components/includes/navbar/navbar.component.ts
@@ -1,55 +1,63 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnChanges ,OnInit{
-  
-@Input() title !:string; 
-@Input() isAdmin!:boolean;
-ngOnInit(): void {
-  if(this.isAdmin){
-    const searchIcon= document.getElementById('search')as HTMLElement;
-  const searchInput= document.getElementById('input-search') as HTMLElement;
-  const header= document.getElementById('header') as HTMLElement;
-  const searchAppear=document.createElement('div') as HTMLElement;
-   
-  header.appendChild(searchAppear); 
-  if(searchIcon!=null){
-    searchIcon.addEventListener('click',function(){ 
-      if(window.innerWidth<=530){
-        this.parentElement?.classList.add('appearSearchDiv');
-        (this.parentElement)?.classList.remove('search-bar');
-       searchInput?.setAttribute ('style', 'background:#f4f4f4 !important;display:block !important;');
-       if(this.parentElement)
-       searchAppear.appendChild(this.parentElement);  
+export class NavbarComponent implements OnChanges, OnInit {
+  @Input() title!: string;
+  @Input() isAdmin!: boolean;
+  ngOnInit(): void {
+    if (this.isAdmin) {
+      const searchIcon = document.getElementById('search') as HTMLElement;
+      const searchInput = document.getElementById(
+        'input-search'
+      ) as HTMLElement;
+      const header = document.getElementById('header') as HTMLElement;
+      const searchAppear = document.createElement('div') as HTMLElement;
+      header.appendChild(searchAppear);
+      if (searchIcon != null) {
+        searchIcon.addEventListener('click', function () {
+          if (window.innerWidth <= 530) {
+            this.parentElement?.classList.add('appearSearchDiv');
+            this.parentElement?.classList.remove('search-bar');
+            searchInput?.setAttribute(
+              'style',
+              'background:#f4f4f4 !important;display:block !important;'
+            );
+            if (this.parentElement)
+              searchAppear.appendChild(this.parentElement);
+          } else {
+            this.parentElement?.classList.remove('appearSearchDiv');
+            this.parentElement?.classList.add('search-bar');
+          }
+        });
       }
-    
-  });
-  }
- 
-  const profile  = document.getElementById('profile') as HTMLElement;
-  const searchBar  = document.querySelector(' .search-bar')as HTMLElement;
-  
-  profile.setAttribute ('style', 'display: flex !important;');
-  searchBar.setAttribute ('style', 'display:flex !important;');
-  }else{
-  const profile  = document.getElementById('profile') as HTMLElement;
-  const searchBar  = document.querySelector(' .search-bar')as HTMLElement;
-  
-  profile.setAttribute ('style', 'display: none !important;');
-  searchBar.setAttribute ('style', 'display: none !important;');
-  
-  }
-}
- 
-ngOnChanges(changes: SimpleChanges): void {
-  console.log(changes);
-  this.title=changes['title'].currentValue;
-  this.isAdmin=changes['isAdmin'].currentValue;
- 
-}
 
+      const profile = document.getElementById('profile') as HTMLElement;
+      const searchBar = document.querySelector(' .search-bar') as HTMLElement;
+
+      profile.setAttribute('style', 'display: flex !important;');
+      searchBar.setAttribute('style', 'display:flex !important;');
+    } else {
+      const profile = document.getElementById('profile') as HTMLElement;
+      const searchBar = document.querySelector(' .search-bar') as HTMLElement;
+
+      profile.setAttribute('style', 'display: none !important;');
+      searchBar.setAttribute('style', 'display: none !important;');
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    this.title = changes['title'].currentValue;
+    this.isAdmin = changes['isAdmin'].currentValue;
+  }
 }
